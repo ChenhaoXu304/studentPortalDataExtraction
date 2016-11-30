2a5d8a8b50632b986213e4a58897e6c364e8b4e6
Chenhao Xu <cx473@nyu.edu>
2016-11-30 12:58:44 +0800

    Update annoucementExtractions.py

--- annoucementExtractions.py
+++ annoucementExtractions.py
@@ -1,9 +1,11 @@
 import requests
 import html
 import codecs
+import getpass
 
 
 def getAnnouncements(username,password):
+    #start session
     with requests.Session() as s:
         s=requests.Session()
         init=s.get('https://students.nyuad.nyu.edu/announcements')
@@ -15,10 +17,8 @@ def getAnnouncements(username,password):
 
         print("Login:",login.status_code,login.reason)
         print("Login:",login.url)
-        #print(r1.url)
-        #print(r1.text[:300])
-        #print('Division')
-
+        
+        #As javascript is not supported, automatic redirects can only be done manually here
         inter=s.get('https://students.nyuad.nyu.edu/announcements')
         print("Inter:",inter.status_code,inter.reason)
         #print(inter.url)
@@ -33,42 +33,26 @@ def getAnnouncements(username,password):
         RelayState_ep=page.find('"/>',RelayState_sp)
         RelayState=page[RelayState_sp:RelayState_ep]
         RelayState=html.unescape(RelayState)
-        #print(RelayState)
-        #print(page[RelayState_sp:RelayState_ep])
+        
         SAMLResponse_sp=page.find("SAMLResponse")+21
         SAMLResponse_ep=page.find('"/>',SAMLResponse_sp)
         SAMLResponse=page[SAMLResponse_sp:SAMLResponse_ep]
         SAMLResponse=html.unescape(SAMLResponse)
-        #print(SAMLResponse)
-        #print(page[SAMLResponse_sp:SAMLResponse_ep])
+        
         portal=s.post(post_link,data={"RelayState":RelayState,"SAMLResponse":SAMLResponse
                                })
         print("final:",portal.status_code,portal.reason)
-        #print(portal.text[:600])
-        '''
-        content=portal.text.replace('\u2028','\n')
-        s1=s.get("https://www.google-analytics.com/analytics.js")
-        #print("google-analytics:",s1.status_code,s1.reason)
-        #print(s1.text[:250])
-
-        s2=s.get("https://api.usersnap.com/load/bf1e2b42-1121-428e-b112-c242f39abf42.js")
-        #print("js2:",s2.status_code,s2.reason)
-        #print(s2.text[:250])
-
-        s3=s.get("https://d3mvnvhjmkxpjz.cloudfront.net/js/11748/usersnap2-11748-en.js")
-        #print("js3:",s3.status_code,s3.reason)
-        #print(s3.text[:250])
-        scripts="<script>"+s1.text+s2.text+s3.text+"</script>"
-        scripts=scripts.replace('\xeb',u'\xeb')'''
-
+        
+       
+        #get announcements
         announcements=s.get(
             "https://students.nyuad.nyu.edu/apps/announcements/index",
             data={"_":None,"amount":50,"category":None,"lastRaDate":None,
         "query":None,
         "start":0})
-        #1480247989193
+        
         print("Announcements:",announcements.status_code,announcements.reason)
-        #print(announcements.text[:600])
+        
         ann=announcements.json()
     return ann["announcements"]
 
@@ -82,15 +66,12 @@ def writeAnnouncements(ann,filename):
         fp.write("Content:  "+str(a['message'])+"<br />")
         fp.write("-"*50+"<br />")
     fp.close()
-'''
-def printAnnouncements(ann):
-    for a in ann:
-        print("Title: ",str(a['title']))
-        if isinstance(a['category'],dict):
-            print("Category: "+str(a['category']['name']))
-        print("Content:  "+str(a['message']))
-        print("-"*60)
-'''
+
+username=input("username: ")
+password=getpass.getpass("password: ")
+ann=getAnnouncements(username,password)
+filename=input("filename: ")
+writeAnnouncements(ann,filename)
 
 
 
